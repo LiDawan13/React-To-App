e05ecc28b81915aea4b36af17fd8dddc8d378b20
Zhanlei Li <janeyli13green@gmail.com>
2022-12-30 10:37:09 +0800

    Task area

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid, Box } from '@mui/material';
 import { format } from 'date-fns';
 import { TaskCounter } from '../taskCounter/taskCounter';
+import { Task } from '../task/task';
 
 export const TaskArea = () => {
   return (
@@ -38,9 +39,9 @@ export const TaskArea = () => {
           xs={10}
           md={8}
         >
-          {/* <Task />
           <Task />
-          <Task /> */}
+          <Task />
+          <Task />
         </Grid>
       </Grid>
     </Grid>
